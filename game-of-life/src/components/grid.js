c2cd41f4fdcbd2be174070cd01c15a098773bb92
Alex Kemper <alex.j.kemper@gmail.com>
2020-09-24 11:23:57 -0400

    mvp complete, working on styling now, pushing to master to test a deploy

--- game-of-life/src/components/grid.js
+++ game-of-life/src/components/grid.js
@@ -5,6 +5,8 @@ import { samples } from "./samples";
 const numRows = 50;
 const numCols = 50;
 
+const size = 10;
+
 const operations = [
   [0, 1],
   [0, -1],
@@ -21,12 +23,22 @@ let initialGrid = () => {
   for (let i = 0; i < numRows; i++) {
     rows.push(Array.from(Array(numCols), () => 0));
   }
+  population = 0;
+  for (let i = 0; i < numRows; i++) {
+    for (let k = 0; k < numCols; k++) {
+      if (rows[i][k] > 0) {
+        population++;
+      }
+    }
+  }
   return rows;
 };
 
+let population = 0;
 let generations = 0;
 
 let speeds = [
+  { name: "10x", milleseconds: 100 },
   { name: "4x", milleseconds: 250 },
   { name: "2x", milleseconds: 500 },
   { name: "1x", milleseconds: 1000 },
@@ -44,12 +56,16 @@ function Grid() {
   const gensRef = useRef(generations);
   gensRef.current = generations;
 
+  const popRef = useRef(population);
+  popRef.current = population;
+
   const speedRef = useRef(speed);
   speedRef.current = speed;
 
   const clearGrid = () => {
     setGrid(initialGrid);
     generations = 0;
+    population = 0;
   };
 
   const randomGrid = () => {
@@ -59,33 +75,52 @@ function Grid() {
         Array.from(Array(numCols), () => (Math.random() > 0.2 ? 0 : 1))
       );
     }
+    population = 0;
+    for (let i = 0; i < numRows; i++) {
+      for (let k = 0; k < numCols; k++) {
+        if (rows[i][k] > 0) {
+          population++;
+        }
+      }
+    }
     setGrid(rows);
+  };
+
+  const sampleGrid = (sample) => {
+    setGrid(sample);
+    generations = 0;
   };
 
   const runSimulation = useCallback(() => {
     if (!runningRef.current) {
       return;
-    }
-    setGrid((g) => {
-      return produce(g, (gridCopy) => {
-        for (let i = 0; i < numRows; i++) {
-          for (let k = 0; k < numCols; k++) {
-            let neighbors = 0;
-            operations.forEach(([x, y]) => {
-              const newI = (i + x + numCols) % numCols;
-              const newK = (k + y + numRows) % numRows;
+    } else {
+      setGrid((g) => {
+        return produce(g, (gridCopy) => {
+          population = 0;
+          for (let i = 0; i < numRows; i++) {
+            for (let k = 0; k < numCols; k++) {
+              let neighbors = 0;
+              operations.forEach(([x, y]) => {
+                const newI = (i + x + numCols) % numCols;
+                const newK = (k + y + numRows) % numRows;
 
-              neighbors += g[newI][newK];
-            });
-            if (neighbors < 2 || neighbors > 3) {
-              gridCopy[i][k] = 0;
-            } else if (g[i][k] === 0 && neighbors === 3) {
-              gridCopy[i][k] = 1;
+                neighbors += g[newI][newK];
+              });
+              if (neighbors < 2 || neighbors > 3) {
+                gridCopy[i][k] = 0;
+              } else if (g[i][k] === 0 && neighbors === 3) {
+                gridCopy[i][k] = 1;
+              }
+              if (gridCopy[i][k] > 0) {
+                population++;
+              }
             }
           }
-        }
+        });
       });
-    });
+    }
+
     generations++;
     setTimeout(runSimulation, speedRef.current);
   }, [speed]);
@@ -121,13 +156,13 @@ function Grid() {
         </button>
       ))}
       {samples.map((item) => (
-        <button onClick={() => setGrid(item.array)}>{item.name}</button>
+        <button onClick={() => sampleGrid(item.array)}>{item.name}</button>
       ))}
       <div
-        className="App"
+        className="container"
         style={{
           display: "grid",
-          gridTemplateColumns: `repeat(${numCols}, 20px)`,
+          gridTemplateColumns: `repeat(${numCols}, ${size}px)`,
         }}
       >
         {grid.map((rows, i) =>
@@ -141,8 +176,8 @@ function Grid() {
                 setGrid(newGrid);
               }}
               style={{
-                width: 20,
-                height: 20,
+                width: size,
+                height: size,
                 backgroundColor: grid[i][k] ? "pink" : undefined,
                 border: "1px solid black",
                 pointerEvents: running ? "none" : "auto",
@@ -152,6 +187,8 @@ function Grid() {
         )}
       </div>
       <div>Generations: {gensRef.current}</div>
+      <div>Population: {popRef.current}</div>
+      <div>{running ? "Running" : "Paused"}</div>
     </>
   );
 }
